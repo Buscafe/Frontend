04deffd33f1c246ba26200862f156a49f558cd6e
Igor Costa Braz <Igorcbraz1@gmail.com>
2021-07-15 14:19:05 -0300

    Atualização pag compras

--- JS/localizador.js
+++ JS/localizador.js
@@ -60,3 +60,20 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   );
   infoWindow.open(map);
 }
+
+// Places API
+var searchInput = 'search_input';
+ 
+$(document).ready(function () {
+ var autocomplete;
+ autocomplete = new google.maps.places.Autocomplete((document.getElementById(searchInput)), {
+  types: ['geocode'],
+  /*componentRestrictions: {
+   country: "USA"
+  }*/
+ });
+  
+ google.maps.event.addListener(autocomplete, 'place_changed', function () {
+  var near_place = autocomplete.getPlace();
+ });
+});
