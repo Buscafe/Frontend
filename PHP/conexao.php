f78ab3cfbaad5c5ec854521958dfaf2d3989b5a1
Igor Costa Braz <Igorcbraz1@gmail.com>
2021-09-07 12:08:11 -0300

    Troca de conexao db2

--- PHP/conexao.php
+++ PHP/conexao.php
@@ -1,9 +1,24 @@
 <?php
-define('HOST', 'remotemysql.com'); 
-define('USUARIO', 'tc4icCf8rc'); 
-define('SENHA', 'S1xbxi9Jjf'); 
-define('DB', 'tc4icCf8rc'); 
+define('HOST', 'sql10.freesqldatabase.com'); #SEVER
+define('USUARIO', 'sql10435079'); #USERNAME
+define('SENHA', 'avKxLxLxDY'); #PASSWORD
+define('DB', 'sql10435079'); #DATABASE NAME
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
 
+/*
+    =========== DATABASE 1 ============
+    Username: tc4icCf8rc
+    Database name: tc4icCf8rc
+    Password: S1xbxi9Jjf
+    Server: remotemysql.com
+    Port: 3306
+    =========== DATABASE 2 ============
+    Username: sql10435079
+    Database name: sql10435079
+    Database password: avKxLxLxDY
+    Sever: sql10.freesqldatabase.com
+    Port : 3306
+    ===================================
+*/
 ?>
