62d892960f7890242ccbe04d64d646d9ecc3e9b5
Igor Costa Braz <Igorcbraz1@gmail.com>
2021-09-22 15:39:24 -0300

    new database

--- PHP/conexao.php
+++ PHP/conexao.php
@@ -1,8 +1,8 @@
 <?php
-define('HOST', 'sql10.freesqldatabase.com'); #SEVER
-define('USUARIO', 'sql10435079'); #USERNAME
-define('SENHA', 'avKxLxLxDY'); #PASSWORD
-define('DB', 'sql10435079'); #DATABASE NAME
+define('HOST', 'buscafe-sever.mysql.database.azure.com'); #SEVER
+define('USUARIO', 'buscafeADM@buscafe-sever'); #USERNAME
+define('SENHA', 'SDCbuscafe240621'); #PASSWORD
+define('DB', 'login'); #DATABASE NAME
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
 
@@ -20,5 +20,10 @@ $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível 
     Sever: sql10.freesqldatabase.com
     Port : 3306
     ===================================
+    define('HOST', 'b2v61ynpmgla8ebsptdy-mysql.services.clever-cloud.com'); #SEVER
+    define('USUARIO', 'ugmbuylznvbkefpl'); #USERNAME
+    define('SENHA', 'vKF69URcVuvGnTBLMZwc'); #PASSWORD
+    define('DB', 'b2v61ynpmgla8ebsptdy'); #DATABASE NAME
+
 */
 ?>
