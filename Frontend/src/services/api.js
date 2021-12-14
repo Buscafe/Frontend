a5a88985b08836f4336f0f74792acdcff24a18f5
Igor <igorcbraz1@gmail.com>
2021-12-14 13:53:09 -0300

    Add cloud database

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,11 +1,5 @@
 import axios from 'axios';
 
-// const token = localStorage.getItem('Token');
-
 export const api = axios.create({
     baseURL: "http://localhost/Buscafe/Backend",
 });
-
-// if(token){
-//     api.defaults.headers['Authorization'] = `Bearer ${token}`;
-// }
