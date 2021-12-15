66144329a2c4b5b1a8ecf42c6c9c26bb445cf1fe
Igor <igorcbraz1@gmail.com>
2021-12-15 16:54:51 -0300

    Line data add landing page

--- Frontend/src/Components/Landing/Footer/Footer.jsx
+++ Frontend/src/Components/Landing/Footer/Footer.jsx
@@ -3,28 +3,28 @@ import { Button } from '../../Button/Button';
 
 import './footer.css'
 
-export default function Footer(){
+export function Footer(){
   return(
-<div>
-    <div className="underMobile">
-      <img id="logo-buscafe" src={logobuscafe} alt="Logo do buscafe"/> 
-    </div>
+    <div id="footerContainer">
+        <div className="underMobile">
+          <img id="logo-buscafe" src={logobuscafe} alt="Logo do buscafe"/> 
+        </div>
 
-    <div className="FooterTxt">
-      <h1 id="buttonTop">Ajudando você a</h1>
-    </div>
-    
-    <div className='buttonLocation'>
-      <Button className="btnLocaliza" id="localizar" location="*">Localizar</Button>
-    </div>
-     
-    <div className="FooterTxt">
-      <h1 id="buttonTop">
-        Buscamos inovar o meio religioso, trazendo soluções 
-        tecnológicas imersivas para o dia a dia das instituições religiosas.
-      </h1>
-    </div>
-     
-</div>    
+        <div className="FooterTxt">
+          <h1 id="buttonTop">Ajudando você a</h1>
+        </div>
+        
+        <div className='buttonLocation'>
+          <Button className="btnLocaliza" id="localizar" location="*">Localizar</Button>
+        </div>
+        
+        <div className="FooterTxt">
+          <h1 id="buttonTop">
+            Buscamos inovar o meio religioso, trazendo soluções 
+            tecnológicas imersivas para o dia a dia das instituições religiosas.
+          </h1>
+        </div>
+        
+    </div>    
   )
 }
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,15 +1,17 @@
 import { Entrada } from "../Components/Landing/Entrada/Entrada.jsx";
 import { Localize } from "../Components/Landing/Localize/Localize.jsx";
-import { Celular  } from "../Components/Landing/Celular/celular.jsx";
-import  Footer    from "../Components/Landing/Footer/Footer.jsx";
- 
+import { Celular } from "../Components/Landing/Celular/celular.jsx";
+import { Footer } from "../Components/Landing/Footer/Footer.jsx";
+import { Dados } from "../Components/Landing/Dados/Dados";
+
 export function Home(){
     return(
         <>
            <Entrada/>
            <Localize/>
+           <Dados/>
            <Celular/>
-           <Footer />
+           <Footer/>
         </>
     );
 }
