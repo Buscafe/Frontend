2c7f590071d3086b9a0936066587726fade99065
Gabriel-limadev <gabrielvitor180701@gmail.com>
2022-06-07 15:41:36 -0300

    Organizando código para informar ao usuario quando informações básicas do templo já tiverem sido cadastradas

--- src/Components/Admin/SwitchPage/Pages/About/AboutCreationMode/index.jsx
+++ src/Components/Admin/SwitchPage/Pages/About/AboutCreationMode/index.jsx
@@ -26,7 +26,7 @@ const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 export function AboutCreationMode(){
     const { user, setUser } = useAuth();
-    const { setCurrentPage, setStepCompleted } = useChurches();
+    const { churchAbout, setCurrentPage, setStepCompleted } = useChurches();
     const [room, setRoom] = useState({seats: '', parking: false, accessibility: false, smartphone: '', email: '', facebook: ''})
     const [isLoading, setIsLoading] = useState(false);
     
@@ -77,92 +77,98 @@ export function AboutCreationMode(){
             setIsLoading(false)
         }
     }
-
+    console.log(churchAbout)
     return (
-        <AboutCreationModeStyles>
-            <Alert severity="info">Cadastre algumas informações básicas sobre seu templo!</Alert>
-            <form onSubmit={handleAddAbout}>
-                <ThemeProvider theme={theme}>
-                <div>
+        <>
+        {churchAbout ? (
+            <Alert severity="info">Informações básicas já foram cadastradas!</Alert>
+        ): (
+            <AboutCreationModeStyles>
+                <Alert severity="info">Cadastre algumas informações básicas sobre seu templo!</Alert>
+                <form onSubmit={handleAddAbout}>
+                    <ThemeProvider theme={theme}>
+                    <div>
+                        <TextField 
+                            id="standard-basic" 
+                            label="Quantidade de assentos" 
+                            value={room.seats}
+                            color="primary"
+                            type="number"
+                            inputProps={{ min: 0 }}
+                            variant="standard" 
+                            onChange={e => setRoom(prevRoom=>{
+                                return {...prevRoom, seats: e.target.value}
+                            })} 
+                        />
+
+                        <Checkbox {...label} 
+                            color="primary"
+                            checked={room.parking}
+                            onChange={e => setRoom(prevRoom=>{
+                                return {...prevRoom, parking: e.target.checked}
+                            })} 
+                        />
+                        <label>Tem estacionamento próprio?</label>
+
+                        <Checkbox {...label} 
+                            color="primary"
+                            checked={room.accessibility}
+                            onChange={e => setRoom(prevRoom=>{
+                                return {...prevRoom, accessibility: e.target.checked}
+                            })} 
+                        />
+                        <label>Comporta Acessibilidade?</label>
+                    </div>
+
                     <TextField 
                         id="standard-basic" 
-                        label="Quantidade de assentos" 
-                        value={room.seats}
+                        label="Celular" 
+                        value={formatSmartPhone(room.smartphone)}
+                        placeholder="(99) 99999-9999"
                         color="primary"
-                        type="number"
-                        inputProps={{ min: 0 }}
+                        type="text"
+                        inputProps={{ maxLength: 15 }}
                         variant="standard" 
                         onChange={e => setRoom(prevRoom=>{
-                            return {...prevRoom, seats: e.target.value}
+                            return {...prevRoom, smartphone: e.target.value}
                         })} 
                     />
-
-                    <Checkbox {...label} 
+                    <TextField 
+                        id="standard-basic" 
+                        label="Email" 
+                        value={room.email}
+                        placeholder="exemplo@exemplo.com"
                         color="primary"
-                        checked={room.parking}
+                        type="email"
+                        variant="standard" 
                         onChange={e => setRoom(prevRoom=>{
-                            return {...prevRoom, parking: e.target.checked}
+                            return {...prevRoom, email: e.target.value}
                         })} 
                     />
-                    <label>Tem estacionamento próprio?</label>
-
-                    <Checkbox {...label} 
+                    <TextField 
+                        id="standard-basic" 
+                        label="Página do Facebook" 
+                        value={room.facebook}
+                        placeholder="Copie e cole o endereço da página"
                         color="primary"
-                        checked={room.accessibility}
+                        type="url"
+                        variant="standard" 
                         onChange={e => setRoom(prevRoom=>{
-                            return {...prevRoom, accessibility: e.target.checked}
+                            return {...prevRoom, facebook: e.target.value}
                         })} 
                     />
-                    <label>Comporta Acessibilidade?</label>
-                </div>
-
-                <TextField 
-                    id="standard-basic" 
-                    label="Celular" 
-                    value={formatSmartPhone(room.smartphone)}
-                    placeholder="(99) 99999-9999"
-                    color="primary"
-                    type="text"
-                    inputProps={{ maxLength: 15 }}
-                    variant="standard" 
-                    onChange={e => setRoom(prevRoom=>{
-                        return {...prevRoom, smartphone: e.target.value}
-                    })} 
-                />
-                <TextField 
-                    id="standard-basic" 
-                    label="Email" 
-                    value={room.email}
-                    placeholder="exemplo@exemplo.com"
-                    color="primary"
-                    type="email"
-                    variant="standard" 
-                    onChange={e => setRoom(prevRoom=>{
-                        return {...prevRoom, email: e.target.value}
-                    })} 
-                />
-                <TextField 
-                    id="standard-basic" 
-                    label="Página do Facebook" 
-                    value={room.facebook}
-                    placeholder="Copie e cole o endereço da página"
-                    color="primary"
-                    type="url"
-                    variant="standard" 
-                    onChange={e => setRoom(prevRoom=>{
-                        return {...prevRoom, facebook: e.target.value}
-                    })} 
-                />
-                </ThemeProvider>
-                <Button 
-                    type="submit" id="createAbout" 
-                    onClick={() => setIsLoading(true)}
-                    className={isLoading && 'loading'}
-                    disabled={(room.seats === '') || (room.email === '') ? true : false}
-                >
-                    Cadastrar Informações
-                </Button>
-            </form>
-        </AboutCreationModeStyles>
+                    </ThemeProvider>
+                    <Button 
+                        type="submit" id="createAbout" 
+                        onClick={() => setIsLoading(true)}
+                        className={isLoading && 'loading'}
+                        disabled={(room.seats === '') || (room.email === '') ? true : false}
+                    >
+                        Cadastrar Informações
+                    </Button>
+                </form>
+            </AboutCreationModeStyles>
+        )}
+        </>
     )
 }
