48e9884e25c4eeea00fc389db138bdceeef4a331
Igor <igorcbraz1@gmail.com>
2022-06-16 21:34:35 -0300

    Update new .env

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,14 +3,15 @@ import { getStorage } from "firebase/storage"
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAd44NGKm2g5WgJA50eYIoqj4Fb3MH7xnM",
-  authDomain: "buscafe-ddae6.firebaseapp.com",
-  projectId: "buscafe-ddae6",
-  storageBucket: "buscafe-ddae6.appspot.com",
-  messagingSenderId: "356962829845",
-  appId: "1:356962829845:web:e3d4a89c9b88e62581301c",
-  measurementId: "G-FXMKT8MT0X"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId:  process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 
 const app = initializeApp(firebaseConfig);
 
