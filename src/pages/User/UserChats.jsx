cb6d3a8f917a363de34b48f1be2542fe4cdfa448
Gabriel-limadev <gabrielvitor180701@gmail.com>
2022-06-24 16:02:23 -0300

    Ajustando chat

--- src/pages/User/UserChats.jsx
+++ src/pages/User/UserChats.jsx
@@ -1,19 +1,27 @@
 import { useHistory } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth.js';
+import { useChat } from '../../hooks/useChat.js'
 
 import { Sidebar } from '../../Components/User/Sidebar/Sidebar';
 import Chats from '../../Components/User/Chats/Chats';
 import { Helmet } from 'react-helmet';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { WithoutChurch } from '../../Components/Admin/WithoutChurch/WithoutChurch.jsx';
+
 
 export function UserChats(){
+    const {churches, getChurches} = useChat();
+
     const [clicked, setClicked] = useState(false);
-    const { signed } = useAuth();
+    const { user, signed } = useAuth();
     const history = useHistory();
     
     if(!signed){
       history.push('/Login');
     }
+    useEffect(async () => {
+        await getChurches(user?.id_user);
+    }, []);
 
     return(
         <>
@@ -21,8 +29,14 @@ export function UserChats(){
                 <title>Home | Buscafé</title>
             </Helmet>
             <Sidebar clicked={clicked} setClicked={setClicked}/>
-            <Chats marginLeft={clicked ? 12 : 22}/>
+
+            {churches.code !== 2 ? (
+                <Chats marginLeft={clicked ? 12 : 22}/>
+            ) : (
+                <WithoutChurch marginLeft={clicked ? 12 : 22}>
+                    <h1><span>Filie a uma igreja</span> para ter acesso ao Social!</h1>
+                </WithoutChurch>
+            )}
         </>
     )
 }
-
